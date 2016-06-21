caad222f83622e7a37cd9722e978cba41e238049
Mastere 1 IT <mastere1it@Reda.etudiant.saint-augustin.in.iesanetwork.com>
2016-06-21 14:44:11 +0200

    #2 class Book & BookStore

---
+++ app/MyLibrary/MyLibrary/Book.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  MyLibrary
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 triforce. All rights reserved.
+//
+
+import Foundation
+
+class Book{
+    var author: String = ""
+    var title: String = ""
+    var description: String = ""
+}
---
+++ app/MyLibrary/MyLibrary/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  MyLibrary
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 triforce. All rights reserved.
+//
+
+import Foundation
+
+class BookStore{
+    
+}
